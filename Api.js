a58bf108e1087286c0535ed626a071809db5e945
JonRider <jon@rider.net>
2021-06-07 20:59:53 +0530

    Movies with posters shown

--- Api.js
+++ Api.js
@@ -1,7 +1,15 @@
-import React from "react";
-import { Text } from "react-native";
 import { API_KEY } from "./key";
 
-export default function api() {
-  return <Text>{API_KEY}</Text>;
-}
+const processMovie = (movie) => ({
+  title: movie["Title"],
+  year: movie["Year"],
+  poster: movie["Poster"],
+});
+
+export const find = async (title) => {
+  const searchURL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${title}`;
+  console.log(searchURL);
+  const response = await fetch(searchURL);
+  const search = await response.json();
+  return search["Search"].map(processMovie);
+};
--- screens/MovieListScreen.js
+++ screens/MovieListScreen.js
@@ -1,21 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { Button, View, Text, StyleSheet } from "react-native";
-import Api from "../Api";
+import { Button, View, Text, StyleSheet, Image } from "react-native";
+import { Card } from "react-native-elements";
+import { find } from "../api";
 
 export default function MovieListScreen({ navigation, route }) {
   const [title, setTitle] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  getMovies = async (title) => {
+    const results = await find(title);
+    setMovies([...results]);
+  };
 
   useEffect(() => {
     if (route.params?.title) {
       setTitle(route.params?.title);
+      getMovies(route.params?.title);
     }
   }, [route.params?.title]);
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Movie List</Text>
-      <Api />
-      <Text>{`Your Movie: ${title}`}</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Movie List</Text>
+      <Text style={styles.searchText}>{`You Searched For: ${title}`}</Text>
+      <Card style={styles.card}>
+        <Card.Title>MOVIE RESULTS</Card.Title>
+        <Card.Divider />
+        {movies.map((movie, i) => {
+          return (
+            <View key={i} style={styles.movie}>
+              <Image
+                style={styles.image}
+                resizeMode="cover"
+                source={{ uri: movie.poster }}
+              />
+              <Text style={styles.title}>{movie.title}</Text>
+            </View>
+          );
+        })}
+      </Card>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate("Details")}
@@ -23,3 +46,41 @@ export default function MovieListScreen({ navigation, route }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    flex: 1,
+  },
+  heading: {
+    fontSize: 28,
+    fontWeight: "bold",
+    paddingBottom: 6,
+  },
+  searchText: {
+    fontSize: 18,
+  },
+  movie: {
+    fontSize: 16,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    minWidth: 100,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 3,
+  },
+  image: {
+    borderRadius: 10,
+    width: 60,
+    height: 80,
+  },
+  title: {},
+});
